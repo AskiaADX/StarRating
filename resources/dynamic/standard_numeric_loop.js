2c4916c91d0fda4976eaace74b72fc92a097fc2e
jeromed <jeromed@askia.com>
2017-07-27 11:02:12 +0200

    v2.0.0

--- resources/dynamic/standard_numeric_loop.js
+++ resources/dynamic/standard_numeric_loop.js
@@ -1,7 +1,7 @@
-﻿{% 
+{% 
 Dim i 
 Dim inputName
-Dim ar = CurrentQuestion.ParentLoop.AvailableResponses
+Dim ar = CurrentQuestion.ParentLoop.Answers
 Dim numberOfStars = CurrentADC.PropValue("numberOfStars").ToNumber()
 
 For i = 1 To ar.Count 
